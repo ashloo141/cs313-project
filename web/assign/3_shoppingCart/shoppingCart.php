e9d9aa6277bc7feb892230178519c94557cd8b74
ashloo141 <loo16001@byui.edu>
2019-10-06 12:20:30 -0600

    add directories

--- web/assign/3_shoppingCart/shoppingCart.php
+++ web/assign/3_shoppingCart/shoppingCart.php
@@ -4,21 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <title> PosterVille </title>
-    <link rel="stylesheet" type="text/css" href="design.css">]
+    <link rel="stylesheet" type="text/css" href="design.css">
     <meta name = "viewport" content = "width=device-width, initial-scale=1, maximum-scale=1">
 </head>
 
 <body>
 
     <header>
-        <h1 style = "margin-top: 100px"> BuyStuff.com </h1>
-        <p> Welcome to PosterVille </p>
+        <h1 style = "margin-top: 100px"> Welcome to PosterVille </h1>
+        <p> Where you can buy all kinds of posters. </p>
     </header>
     
     <form action = "shoppingcart.php" onload="">
                 
         <div>
-            Disney's Beauty and the Beast= $10.00
+            Disney's Beauty and the Beast= $15.00
             <input type = "checkbox" id = "b&b" onclick="check(this)">
             <br>
 
@@ -26,7 +26,7 @@
             <input type = "checkbox" id = "iron" onclick="check(this)">
             <br>
 
-            Pokemon's Pikachu  = $30.00
+            Pokemon's Pikachu  = $20.00
             <input type = "checkbox" id = "pika" onclick="check(this)">
             <br>
 
@@ -43,49 +43,6 @@
                 });
             ?>
         </div>
-        
-        <br>
-        Total:
-        <br>
-        <p id="calc"></p>
-        
-        <br>
-        Credit Card Type:
-        <br>
-        
-        <div>
-            <input type = "radio" id = "visa">
-            Visa
-            <br>
-
-            <input type = "radio" id = "mastercard">
-            MasterCard
-            <br>
-
-            <input type = "radio" id = "amexpress">
-            American Express
-            <br>
-        </div>
-        
-        CC Number: 
-        <input type = "text" id = "ccn" placeholder = "xxxx xxxx xxxx xxxx" size="16" maxlength="20" onchange="isCCNvalid()">
-        <br>
-        <span id="validccn" style='color:red' hidden="hidden">Invalid credit card number
-        <br></span>
-        
-        CC Expiration: 
-        <input type = "text" id = "expm" placeholder = "mm" size="2" maxlength="2"> / 
-        <input type = "text" id = "expyr" placeholder = "yyyy" size="4" maxlength="4">
-        
-        <br>
-        <input type = "reset" value = "Reset" onreset="default">
-        <br>
-        
-        <br>
-        <input type = "submit" value = "Submit" onsubmit = "validateForm()" >
-        <br>
-        
-        <br>
         
     </form>
 </body>
---
+++ web/team/ten_div.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<? php 
+    for($i = 0; $i < 10; i++) {
+        if($i % 2 == 0){ echo "style='color: blue' "}
+        ech<div id=<?php echo"$i"> Div <? php echo "$i" ?> </div>
+        
+    }
+?>
