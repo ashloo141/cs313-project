8c24e1815b214e44bf9a606efff4e9966f577914
ashloo141 <loo16001@byui.edu>
2019-10-08 13:55:20 -0600

    fix css

--- web/3_shoppingCart/clist.php
+++ web/3_shoppingCart/clist.php
@@ -1,12 +1,12 @@
 <?php 
     $total = 0.00;
 
-    echo "Items in Cart: " . count($_SESSION["cart"]) . "<br>";
+    echo "Items in Cart: " . count($_SESSION["cart"]) . "<br><br>";
 
     foreach ($_SESSION["cart"] as $item => $val) {
-        echo "<div id='" . $item . "' style='text-align:left;' > <button type='submit'> Remove </button> " . $item . ": $10.00 </div> <br>";
+        echo "<div id='" . $item . "' style='text-align:left; margin-left: 40px;' > <button type='submit'> Remove </button> " . $item . ": $10.00 </div> <br>";
         $total += 10;
     }
 
-    echo "Total: $" . $total . ".00";
+    echo "Total: $" . $total . ".00 <br><br>";
 ?>
