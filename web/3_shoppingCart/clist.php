8db67696d7e433db4d2d8200beb0883a531b063d
ashloo141 <loo16001@byui.edu>
2019-10-08 13:47:11 -0600

    more hacking

--- web/3_shoppingCart/clist.php
+++ web/3_shoppingCart/clist.php
@@ -1,7 +1,7 @@
 <?php 
     $total = 0.00;
 
-    echo "Items in Cart: " . count($_SESSION["cart"]);
+    echo "Items in Cart: " . count($_SESSION["cart"]) . "<br>";
 
     foreach ($_SESSION["cart"] as $item => $val) {
         echo "<div id='" . $item . "'> <button type='submit'> Remove </button> " . $item . ": $10.00 </div> <br>";
