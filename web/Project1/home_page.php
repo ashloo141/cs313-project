15b0f7275084d17aab3e432b5947bdd5a1170a49
ashloo141 <loo16001@byui.edu>
2019-10-22 11:41:48 -0600

    design & style

--- web/Project1/home_page.php
+++ web/Project1/home_page.php
@@ -20,12 +20,12 @@
     <div class = container>
         <form action = "home_page.php" method=POST>
         <?php 
-            foreach ($db->query('SELECT item_name, item_price, itam_info FROM food_items;') as $row) {
+            foreach ($db->query('SELECT item_name, item_price, item_info FROM food_items;') as $row) {
                 $food = $row['item_name'];
                 $info = $row['item_info'];
                 $price = $row['item_price'];
 
-                echo '<b>$food</b>: $ ' . $price . ' - ' . $info;
+                echo "<b>$food</b>: $ $price - $info";
                 echo "<br/>";
                 echo "<br/>";
             }
