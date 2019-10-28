ad8e22d90c1a690bae1f34a6b8fe8d201194dc03
ashloo141 <loo16001@byui.edu>
2019-10-27 19:35:32 -0600

    login

--- web/Project1/login.php
+++ web/Project1/login.php
@@ -2,43 +2,43 @@
 <!DOCTYPE html>
 <html lang="en">   
 
-<head>
-    <meta charset="UTF-8">
-    <title> Toasties </title>
-    <link rel="stylesheet" type="text/css" href="design.css">
-    <meta name = "viewport" content = "width=device-width, initial-scale=1, maximum-scale=1">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title> Toasties </title>
+        <link rel="stylesheet" type="text/css" href="design.css">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1, maximum-scale=1">
+    </head>
 
-<body>
-    <header>
-        <h1> Smith's Toasties</h1>
-        <p> Fresh. Fast. Freaking Good. </p>
-        <p> IBC </p>
-    </header>
-    
-    <p> Don't have an account? <a href="register.php"> Create Account </a> </p>
+    <body>
+        <header>
+            <h1> Smith's Toasties</h1>
+            <p> Fresh. Fast. Freaking Good. </p>
+            <p> IBC </p>
+        </header>
+        
+        <p> Don't have an account? <a href="register.php"> Create Account </a> </p>
 
-    <form action = "result.php" class=container method=POST>
-        
-        Username:
-        <br>
-        <input type = "text" name = "user" value = "">
-        <br>
-        
-        Password:
-        <br>
-        <input type = "text" name = "pswd" value = "">
-        <br>
+        <div class=container>
+            <form action = "result.php" method=POST>
+                
+                Username:
+                <br>
+                <input type = "text" name = "user" value = "">
+                <br>
+                
+                Password:
+                <br>
+                <input type = "text" name = "pswd" value = "">
+                <br>
 
-        <br>
-        <button type = "submit" value = "submit" >Sign in </button>
-        <br>
-        
-        <br>
-        
-    </form>
-
-</body>
+                <br>
+                <button type = "submit" value = "submit" >Sign in </button>
+                <br>
+                <br>
+                
+            </form>
+        </div>
+    </body>
 </html>
 
 
