7f22ae442291a1ebcef81ed6dabf7dfe4b04f906
ashloo141 <loo16001@byui.edu>
2019-10-27 21:53:41 -0600

    login

--- web/Project1/login.php
+++ web/Project1/login.php
@@ -25,13 +25,13 @@
                 <input type='hidden' name='submitted' id='submitted' value='1'/>
 
                 <label for='username' >UserName*:</label>
-                <input type='text' name='username' id='username'  maxlength="50" />
-                
+                <input type='text' name='username' id='username'  maxlength="20" />
+                <br/>
                 <label for='password' >Password*:</label>
-                <input type='password' name='password' id='password' maxlength="50" />
+                <input type='password' name='password' id='password' maxlength="30" />
                 
 
-                <input type='submit' name='Submit' value='Submit' />
+                <input type='submit' name='Submit' value='Sign in' />
                 
                 </fieldset>
             </form>
