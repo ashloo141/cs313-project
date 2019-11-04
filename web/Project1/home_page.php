57f3dda77efe90305fd10d0623706a763071b621
ashloo141 <loo16001@byui.edu>
2019-11-04 14:09:39 -0700

    fix footer

--- web/Project1/home_page.php
+++ web/Project1/home_page.php
@@ -116,8 +116,8 @@
         <footer>
             <hr>
             <b> Open Times:</b>
-            <p> M-F except Tuesday: 11am - 2pm </p>
-            <p> Tuesday: 10:30am - 11:30am & 12:15pm - 2:30pm </p>
+            <p> <i> M-F except Tuesday:</i> 11am - 2pm </p>
+            <p> <i> Tuesday:</i> 10:30am - 11:30am & 12:15pm - 2:30pm </p>
         </footer>
     </body>
 </html>
