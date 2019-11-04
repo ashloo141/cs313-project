93b7f76fbc32343be2ecb1c47f557caeb8e2f10b
ashloo141 <loo16001@byui.edu>
2019-11-04 13:25:02 -0700

    margins

--- web/Project1/home_page.php
+++ web/Project1/home_page.php
@@ -19,11 +19,11 @@
             <h3> Menu </h3>
             <div id="pics">
                 <img src="pics/all_zoom.jpg" width="30%" alt="Paninis">
-                <img src="pics/nutella.jpg" width="150px" alt="Paninis">
-                <img src="pics/all_stack.jpg" width="150px" alt="Paninis">
-                <img src="pics/smore_side.jpg" width="150px" alt="Paninis">
-                <img src="pics/soup1.jpg" width="150px" alt="Soup">
-                <img src="pics/soup2.jpg" width="150px" alt="Soup">
+                <img src="pics/nutella.jpg" width="30%" alt="Paninis">
+                <img src="pics/all_stack.jpg" width="30%" alt="Paninis">
+                <img src="pics/smore_side.jpg" width="30%" alt="Paninis">
+                <img src="pics/soup1.jpg" width="30%" alt="Soup">
+                <img src="pics/soup2.jpg" width="30%" alt="Soup">
             </div>
 
             <div id="menu">
