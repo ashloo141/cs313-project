5de2214f84fdb8789ee970b62e7046b69d64d5c4
ashloo141 <loo16001@byui.edu>
2019-11-05 14:51:26 -0700

    guest

--- web/Project1/guest.php
+++ web/Project1/guest.php
@@ -17,23 +17,6 @@
     
     <h1> Guest checkout.</h1>
 
-    <?php require '../../db/db-connect.php'; ?>
-<!DOCTYPE html>
-<html lang="en">   
-
-<head>
-        <meta charset="UTF-8">
-        <title> Toasties </title>
-        <link rel ="icon" href ="pics/icon.png">
-        <link rel ="stylesheet" type ="text/css" href ="design.css">
-        <meta name ="viewport" content ="width=device-width, initial-scale=1, maximum-scale=1">
-    </head>
-
-    <body>
-        <header>
-            <h1> <a class =inv_link href= "home_page.php"><img src="pics/logo.png" width="400px" alt="logo"> </a> </h1>
-        </header>
-    
     <br> 
     <div class = container>
 
